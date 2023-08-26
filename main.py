26beb0b15bb91cd0e7e3522771d3c9ba10cfed8b
tvoiugas <tvoiugas@gmail.com>
2023-08-27 00:47:35 +0600

    Temporary logs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 from fastapi import FastAPI, HTTPException, Body
+from middleware import log_requests_middleware
 
 from models import Order
 
 app = FastAPI()
+
+app.middleware("http")(log_requests_middleware)
 
 @app.post("/create_order/", response_model = Order)
 def send_order(order: Order):
