4eec73fc2ee0add6392fd00fe9aa7fdda968a0fd
tvoiugas <tvoiugas@gmail.com>
2023-08-27 00:05:02 +0600

    Some changes

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ from models import Order
 app = FastAPI()
 
 @app.post("/create_order/", response_model = Order)
-def send_order(order: Order = Body(...)):
+def send_order(order: Order):
     
     print(order)
-
-    with open("last_order.txt", "w") as file:
-        file.write(str(order))
-
     return order
